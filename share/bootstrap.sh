5bf128b9224f76acf84ac90ee15c4c46db7465a1
Tien Nguyen <tien.nguyen@lumenci.com>
2020-11-17 09:24:49 -0600

    add new env to extract pdf to html

--- share/bootstrap.sh
+++ share/bootstrap.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
+add-apt-repository ppa:webupd8team/java
+add-apt-repository ppa:openjdk-r/ppa 
 apt-get install git -y
 apt-get update
 apt-get upgrade -y
+apt-get install libmagickwand-dev -y
+apt-get install openjdk-8-jdk -y
 
 miniconda=Miniconda3-py37_4.8.2-Linux-x86_64.sh
 cd /vagrant
