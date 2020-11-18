9b2c44f172299204239ad0fcb22c6d596df5eadd
Tien Nguyen <tien.nguyen@lumenci.com>
2020-11-18 08:50:34 -0600

    using compact ubuntu

--- Vagrantfile
+++ Vagrantfile
@@ -1,6 +1,6 @@
 Vagrant.configure(2) do |config|
-  config.vm.box = "ubuntu/trusty64"
-  config.vm.box_version = "20190514.0.0"
+  config.vm.box = "ubuntu/xenial64"
+  config.vm.box_version = "20201117.0.0"
   config.vm.network "forwarded_port", guest: 8888, host: 8888
   #config.vm.network  "public_network", bridge: "Realtek 8822BE Wireless LAN 802.11ac PCI-E NIC"
   config.vm.network  "public_network", bridge: "Hyper-V Virtual Ethernet Adapter"
--- share/bootstrap.sh
+++ share/bootstrap.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 add-apt-repository ppa:webupd8team/java
 add-apt-repository ppa:openjdk-r/ppa 
-apt-get install git -y
+apt-get install -y git 
 apt-get update
 apt-get upgrade -y
-apt-get install libmagickwand-dev -y
-apt-get install openjdk-8-jdk -y
+apt-get install -y pdf2htmlex libmagickwand-dev unzip openjdk-8-jre-headless
 
 miniconda=Miniconda3-py37_4.8.2-Linux-x86_64.sh
 cd /vagrant
@@ -14,6 +13,7 @@ if [[ ! -f $miniconda ]]; then
 fi
 chmod +x $miniconda
 ./$miniconda -b -p /opt/anaconda
+rm -R pdf2htmlEX-0.16.0-poppler-0.62.0-ubuntu-18.04
 
 cat >> /home/vagrant/.bashrc << END
 PATH=/opt/anaconda/bin:\$PATH
